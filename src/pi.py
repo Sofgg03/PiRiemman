58a2d19deed2335e0007bffb79df6515412b3d5a
melaniec <melanie.cervantes@ucr.ac.cr>
2024-09-16 14:24:39 -0600

    First commit

---
+++ src/pi.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def func(x):
+    f=float(4/1 + x²)
+    return f
+
+def defint(a,b,n,fx)
+    valorpi = 0.0
+    for i in range (0,n+1)
+     valorpi +=  float(((b-a/n)/2*fx))
+     return valorpi
+
+ print def
